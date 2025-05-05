83dc9e59991db83f3af29e9df152c9c253be5f92
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-05-05 09:35:59 +0300

    set up basic project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './core/pages/not-found/not-found.component';
+import { LoginComponent } from './core/pages/login/login.component';
+import { LandingComponent } from './feature/pages/landing/landing.component';
+import { HomeComponent } from './feature/pages/home/home.component';
+import { RegisterComponent } from './core/pages/register/register.component';
+import { ForgetPasswordComponent } from './core/pages/forget-password/forget-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"landing", component:LandingComponent},
+  {path:"home", component:HomeComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"forgetPassword", component:ForgetPasswordComponent},
+  {path:"**", component:NotFoundComponent}
+];
