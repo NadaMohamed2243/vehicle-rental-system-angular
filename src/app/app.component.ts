1cf7dc52ff79aff26bb8e717953ca2af05383ada
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-05-06 01:19:50 +0300

    Hero Banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './shared/components/ui/card/card.component';
+import { HeroComponent } from "./shared/components/ui/hero/hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
