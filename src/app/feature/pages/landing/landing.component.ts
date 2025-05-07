d0b69852b14b0aaecb2e05deb55e03e99554f621
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-05-08 01:51:02 +0300

    Reorganize component placement for better structure

--- src/app/feature/pages/landing/landing.component.ts
+++ src/app/feature/pages/landing/landing.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from '../../../core/layout/navbar/navbar.component';
 import { HeroComponent } from '../../../shared/components/ui/hero/hero.component';
 import { BrandIconsComponent } from './components/brand-icons/brand-icons.component';
 import { FeaturedCarsComponent } from "../../components/featured-cars/featured-cars.component";
+import { CarTypesComponent } from "../../components/car-types/car-types.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [NavbarComponent, HeroComponent, BrandIconsComponent, FeaturedCarsComponent],
+  imports: [NavbarComponent, HeroComponent, BrandIconsComponent, FeaturedCarsComponent, CarTypesComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
