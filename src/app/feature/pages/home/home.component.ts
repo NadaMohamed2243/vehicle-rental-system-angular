6ddd6eb3d4002bb38a24499a18c64e2b85a72d12
dina <dinaelhadidy966@gmail.com>
2025-05-14 14:49:14 +0300

    Refactor Home component to use Layout component and improve HTML structure

--- src/app/feature/pages/home/home.component.ts
+++ src/app/feature/pages/home/home.component.ts
@@ -1,21 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { CardComponent } from "../../../shared/components/ui/card/card.component";
+import { CardComponent } from '../../../shared/components/ui/card/card.component';
 import { Car } from '../../../core/interfaces/car';
 import { CarsService } from '../../../core/services/cars.service';
-
-
+import { LayoutComponent } from '../../../core/pages/layout/layout.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent],
+  imports: [CardComponent, LayoutComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
-  cars!:Car[]
-  _carsService=inject(CarsService)
+export class HomeComponent implements OnInit {
+  cars!: Car[];
+  _carsService = inject(CarsService);
 
   ngOnInit(): void {
-    this.cars=this._carsService.getCars();
+    this.cars = this._carsService.getCars();
   }
 }
