b949133e3c72171617d9126b7d4456081fe49f81
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-05-14 17:56:10 +0300

    why_choose_us component add

--- src/app/feature/pages/landing/landing.component.ts
+++ src/app/feature/pages/landing/landing.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../core/layout/navbar/navbar.component';
 import { HeroComponent } from '../../../shared/components/ui/hero/hero.component';
 import { BrandIconsComponent } from './components/brand-icons/brand-icons.component';
+import { HowItWorksComponent } from "./components/how-it-works/how-it-works.component";
+import { WhyChooseRentlyComponent } from './components/why-choose-rently/why-choose-rently.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [NavbarComponent,HeroComponent,BrandIconsComponent],
+  imports: [NavbarComponent, HeroComponent, BrandIconsComponent, HowItWorksComponent , WhyChooseRentlyComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
