d6f4e367b4dcd42ad42160d14fbf88f7d2641db7
dina <dinaelhadidy966@gmail.com>
2025-05-14 10:35:17 +0300

    Add car slider component with Swiper integration and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { register } from 'swiper/element/bundle';
+import { CarSliderComponent } from './feature/components/ui/car-slider/car-slider.component';
+
+register();
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    InputTextModule,
+    FormsModule,
+    IconFieldModule,
+    InputIconModule,
+    ButtonModule,
+    CarSliderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'VehicleRentalSystem';
