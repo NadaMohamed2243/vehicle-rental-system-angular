364fb1e0a90832fd5d8816da596e090ac5bc812a
dina <dinaelhadidy966@gmail.com>
2025-05-14 10:38:29 +0300

    Add Cars page with routing, styles, and UI enhancements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { LandingComponent } from './feature/pages/landing/landing.component';
 import { HomeComponent } from './feature/pages/home/home.component';
 import { RegisterComponent } from './core/pages/register/register.component';
 import { ForgetPasswordComponent } from './core/pages/forget-password/forget-password.component';
+import { CarsComponent } from './feature/pages/cars/cars.component';
 
 export const routes: Routes = [
-  {path:"landing", component:LandingComponent},
-  {path:"home", component:HomeComponent},
-  {path:"login", component:LoginComponent},
-  {path:"register", component:RegisterComponent},
-  {path:"forgetPassword", component:ForgetPasswordComponent},
-  {path:"**", component:NotFoundComponent}
+  { path: 'landing', component: LandingComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'cars', component: CarsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgetPassword', component: ForgetPasswordComponent },
+  { path: '**', component: NotFoundComponent },
 ];
--- src/app/feature/pages/cars/cars.component.ts
+++ src/app/feature/pages/cars/cars.component.ts
@@ -4,16 +4,54 @@ import { FilterComponent } from '../../components/ui/filter/filter.component';
 import { Car } from '../../../core/interfaces/car';
 import { CarsService } from '../../../core/services/cars.service';
 import { CardComponent } from '../../../shared/components/ui/card/card.component';
+import { DrawerModule } from 'primeng/drawer';
+import { AvatarModule } from 'primeng/avatar';
+import { CarSliderComponent } from '../../components/ui/car-slider/car-slider.component';
+import { TabsModule } from 'primeng/tabs';
+import { DividerModule } from 'primeng/divider';
+import { DatePickerModule } from 'primeng/datepicker';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
   selector: 'app-cars',
-  imports: [LayoutComponent, FilterComponent, CardComponent],
+  imports: [
+    LayoutComponent,
+    FilterComponent,
+    CardComponent,
+    DrawerModule,
+    AvatarModule,
+    CarSliderComponent,
+    TabsModule,
+    DividerModule,
+    DatePickerModule,
+    RadioButtonModule,
+    FormsModule,
+    ButtonModule,
+    ToggleSwitchModule,
+  ],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css',
 })
 export class CarsComponent implements OnInit {
+  visible: boolean = false;
+  checked: boolean = false;
+
   cars!: Car[];
   _carsService = inject(CarsService);
+  isFavorite = false;
+  insurance: string[] = [
+    'No insurance',
+    'Vehicle protection',
+    '3rd Party liability',
+  ];
+  selectedInsurance: string = this.insurance[0];
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+  }
 
   ngOnInit(): void {
     this.cars = this._carsService.getCars();
