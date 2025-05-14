ee3701dccb360a7984acedb8f2f781d1cbfe242c
dina <dinaelhadidy966@gmail.com>
2025-05-14 10:30:30 +0300

    Add filter component with layout, styles, and functionality

---
+++ src/app/feature/components/ui/filter/filter.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { DividerModule } from 'primeng/divider';
+import { AccordionModule } from 'primeng/accordion';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [
+    InputTextModule,
+    FormsModule,
+    IconFieldModule,
+    InputIconModule,
+    ButtonModule,
+    ToggleSwitchModule,
+    DividerModule,
+    AccordionModule,
+    CheckboxModule,
+    RadioButtonModule,
+    InputNumberModule,
+  ],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css',
+})
+export class FilterComponent {
+  visible: boolean = false;
+  selectedBrands: any[] = [];
+  selectedBodyTypes: any[] = [];
+  selectedTransmissionTypes: any[] = [];
+  selectedFuelTypes: any[] = [];
+  checked: boolean = false;
+  brands: String[] = [
+    'Toyota',
+    'Volkswagen',
+    'Ford',
+    'Honda',
+    'Chevrolet',
+    'Nissan',
+    'Hyundai',
+    'Kia',
+    'BMW',
+    'Mercedes',
+  ];
+  carBodyTypes: String[] = [
+    'Sedan',
+    'Hatchback',
+    'Coupe',
+    'Convertible',
+    'Wagon',
+    'Sports Car',
+  ];
+  transmissionTypes: String[] = ['Any', 'Automatic', 'Manual'];
+  fuelTypes: String[] = ['Gasoline', 'Diesel', 'Electric', 'Hybrid'];
+}
