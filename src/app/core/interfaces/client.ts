d1c2602d202e1607e09c2a0e1785706c4f902d24
Hagar Elmadany <hagarelmadany@gmail.com>
2025-06-12 03:14:55 +0300

    admin-accept-client

--- src/app/core/interfaces/client.ts
+++ src/app/core/interfaces/client.ts
@@ -1,12 +1,13 @@
-export interface Client {
-    id: number;
-    name: string;
-    email: string;
-    location: Location
-    status: 'pending' | 'approved';
-    license: string;
-}
 
-export interface Location {
-  city: string
+export interface Client {
+  _id: string;
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  location: string;
+  lat: number;
+  lng: number;
+  driver_license: string;
+  verification_status: 'pending' | 'approved' | 'rejected'; 
 }
