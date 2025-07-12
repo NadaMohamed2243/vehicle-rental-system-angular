df03ef42972da229c53ccd0ed88988305753590b
dina <dinaelhadidy966@gmail.com>
2025-07-12 20:09:28 +0300

    Add label support for links and implement sidebar state persistence

--- src/app/shared/components/ui/sidebar/sidebar.component.ts
+++ src/app/shared/components/ui/sidebar/sidebar.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { links } from '../../../constants/navLinks';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   links = links;
+  isExpanded = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedState = localStorage.getItem('sidebarExpanded');
+      this.isExpanded = savedState === 'true';
+    }
+  }
+
+  toggleSidebar() {
+    this.isExpanded = !this.isExpanded;
+    console.log(this.isExpanded);
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('sidebarExpanded', this.isExpanded.toString());
+    }
+  }
 }
--- src/app/shared/constants/navLinks.ts
+++ src/app/shared/constants/navLinks.ts
@@ -1,31 +1,33 @@
 interface NavLink {
   img: string;
   route: string;
+  label: string; // Add label property
 }
 
 export const links: NavLink[] = [
   {
     img: 'home',
     route: '/home',
+    label: 'Home',
   },
   {
     img: 'car',
     route: '/cars',
-  },
-  {
-    img: 'calendar',
-    route: '/calendar',
+    label: 'Cars',
   },
   {
     img: 'heart',
     route: '/favourites',
+    label: 'Favourites',
   },
   {
     img: 'history',
     route: '/history',
+    label: 'History',
   },
   {
     img: 'user',
     route: '/profile',
+    label: 'Profile',
   },
 ];
