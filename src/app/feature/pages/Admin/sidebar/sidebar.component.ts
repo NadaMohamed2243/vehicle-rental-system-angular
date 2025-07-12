7025103ab5e5f2d434bb27f88afc59227f421455
Hagar Elmadany <hagarelmadany@gmail.com>
2025-07-13 02:03:30 +0300

    Enhance sidebar component with improved toggle functionality and navigation links

--- src/app/feature/pages/Admin/sidebar/sidebar.component.ts
+++ src/app/feature/pages/Admin/sidebar/sidebar.component.ts
@@ -5,18 +5,24 @@ import { RippleModule } from 'primeng/ripple';
 import { AvatarModule } from 'primeng/avatar';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [ RippleModule, AvatarModule,CommonModule,RouterLink,RouterLinkActive],
+  imports: [ RippleModule, AvatarModule,CommonModule,RouterLink,RouterLinkActive , DrawerModule, ButtonModule, SidebarModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  sidebarVisible = false;
+  sidebarVisible = true;
+  hovered = false;
 
   toggleSidebar() {
     this.sidebarVisible = !this.sidebarVisible;
   }
+
+
 
 }
