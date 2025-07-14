616c712facbe340bc115d1cdaa9c05e9505c0fc1
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-07-14 23:21:09 +0300

    edit env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api',
-  baseUrl: 'http://localhost:5000'
-};
 // export const environment = {
-//   production: true,
-//   apiUrl: 'https://lucky-growth-production.up.railway.app/api',
-//   baseUrl: 'https://lucky-growth-production.up.railway.app',
+//   production: false,
+//   apiUrl: 'http://localhost:5000/api',
+//   baseUrl: 'http://localhost:5000'
 // };
+export const environment = {
+  production: true,
+  apiUrl: 'https://lucky-growth-production.up.railway.app/api',
+  baseUrl: 'https://lucky-growth-production.up.railway.app',
+};
